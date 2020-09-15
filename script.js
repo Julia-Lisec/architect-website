9994faaec3897b7b541c6e218e975040c4fc3b6a
Julia Lisec <julialisec12@gmail.com>
2020-09-15 05:26:22 +0200

    script for scroll-up btn done

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+//sidebar toggle
 document.querySelector('.hamburger-menu').addEventListener('click', () => {
   document.querySelector('.container').classList.toggle('change');
+});
+
+//scroll-up btn
+document.querySelector('.scroll-btn').addEventListener('click', () => {
+  document.querySelector('html').style.scrollBehavior = 'smooth';
+  setTimeout(() => {
+    document.querySelector('html').style.scrollBehavior = 'unset';
+  }, 1000);
 });
