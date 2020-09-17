79bbf5563c6946179996f54670eab1d1ec688dd6
Julia Lisec <julialisec12@gmail.com>
2020-09-17 11:39:42 +0200

    created script for loading circle timeout

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//loading circles timeout
+window.onload = () => {
+  setTimeout(() => {
+    document.querySelector('body').classList.add('display');
+  }, 4000);
+};
+
 //sidebar toggle
 document.querySelector('.hamburger-menu').addEventListener('click', () => {
   document.querySelector('.container').classList.toggle('change');
